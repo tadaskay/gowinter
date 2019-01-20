ce07e3a77a4bbe584a9f5f571a9e3b6de8ef9411
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-01-20 22:23:13 +0200

    Read and output a message from TCP client

--- winter.go
+++ winter.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
+
+type Client struct {
+	socket net.Conn
+	data   chan []byte
+}
+
+var PORT int = 52000
+
+func receive(client *Client) {
+	for {
+		message := make([]byte, 4096)
+		n, err := client.socket.Read(message)
+		if err != nil {
+			fmt.Println("Error reading from client: ", err)
+			_ = client.socket.Close()
+			break
+		}
+		if n > 0 {
+			client.data <- message[:n]
+		}
+	}
+}
 
 func main() {
-	fmt.Print("Winter is coming")
+	var client *Client
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(PORT))
+	if err != nil {
+		_, _ = fmt.Fprint(os.Stderr, "Error listening for connections", err)
+		os.Exit(1)
+	}
+	conn, err := listener.Accept()
+	if err != nil {
+		_, _ = fmt.Fprint(os.Stderr, "Error connecting: ", err)
+		os.Exit(1)
+	}
+	client = &Client{socket: conn, data: make(chan []byte)}
+	go receive(client)
+	message := <-client.data
+	fmt.Println(string(message))
 }
