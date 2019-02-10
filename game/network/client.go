a80c41a4507ed97e511c42ec6633eddc6c740fc9
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-02-10 12:33:26 +0200

    Create game client emitting game events

---
+++ game/network/client.go
@@ -0,0 +1,43 @@
+package network
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"winter-is-coming/game/event"
+)
+
+type GameClient struct {
+	socket net.Conn
+	Events chan interface{}
+}
+
+func NewClient(conn net.Conn) *GameClient {
+	client := &GameClient{
+		socket: conn,
+		Events: make(chan interface{}),
+	}
+	return client
+}
+
+func (client *GameClient) StartReceiving() {
+	go func() {
+		for {
+			buf := make([]byte, 4096)
+			n, err := client.socket.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading from client:", err)
+				_ = client.socket.Close()
+				break
+			}
+			if n > 0 {
+				message := strings.TrimRight(string(buf[:n]), "\r\n")
+				gameEvent, err := event.Unmarshal(message)
+				if err != nil {
+					fmt.Println("Invalid message from client:", err)
+				}
+				client.Events <- gameEvent
+			}
+		}
+	}()
+}
