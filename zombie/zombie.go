f6f8baab867c06bbeedc6d6e21c0a315a0ad6f78
Tadas Krivickas <tadas.krivickas@gmail.com>
2019-02-10 16:27:26 +0200

    Hide zombie's internals

--- zombie/zombie.go
+++ zombie/zombie.go
@@ -9,44 +9,44 @@ import (
 
 type Zombie struct {
 	Name  string
-	Pos   board.Position
-	state State
+	pos   board.Position
+	state state
 
 	bounds     board.Bounds
 	moveTicker *time.Ticker
 }
 
-type State int
+type state int
 
 const (
-	Initial State = iota
-	Moving
-	Dead
+	initial state = iota
+	moving
+	dead
 )
 
 func New(name string) *Zombie {
 	return &Zombie{
 		Name:  name,
-		state: Initial,
+		state: initial,
 	}
 }
 
 func (z *Zombie) Spawn(bounds board.Bounds) {
 	rand.Seed(time.Now().UnixNano())
-	z.Pos = board.Position{X: rand.Intn(bounds.X)}
+	z.pos = board.Position{X: rand.Intn(bounds.X)}
 	z.bounds = bounds
 	z.moveTicker = time.NewTicker(2 * time.Second)
-	z.state = Moving
+	z.state = moving
 }
 
 func (z *Zombie) HandleShot(pos board.Position) {
-	if z.Pos == pos {
-		z.state = Dead
+	if z.pos == pos {
+		z.state = dead
 	}
 }
 
 func (z *Zombie) Update() {
-	if z.state == Moving {
+	if z.state == moving {
 		z.move()
 	}
 }
@@ -61,28 +61,28 @@ func (z *Zombie) move() {
 	}
 
 	deltaX := rand.Intn(3) - 1
-	newX := z.Pos.X + deltaX
+	newX := z.pos.X + deltaX
 	if newX < 0 {
 		newX = 0
 	} else if newX > z.bounds.X {
 		newX = z.bounds.X
 	}
 
-	z.Pos.X = newX
-	z.Pos.Y += 1
+	z.pos.X = newX
+	z.pos.Y += 1
 
 	if z.IsSouthReached() {
 		z.moveTicker.Stop()
 		return
 	} else {
-		fmt.Println("MOVE", z.Pos)
+		fmt.Println("MOVE", z.pos)
 	}
 }
 
 func (z *Zombie) IsSouthReached() bool {
-	return z.Pos.Y == z.bounds.Y
+	return z.pos.Y == z.bounds.Y
 }
 
 func (z *Zombie) IsDead() bool {
-	return z.state == Dead
+	return z.state == dead
 }
